f086bfc3002d186eb871dc6d45762100c94c2572
Jenya <hypnagonia@gmail.com>
2024-11-12 17:50:42 +0400

    Api (#10)
    grouping by job_seq_number

--- src/api/schema.rs
+++ src/api/schema.rs
@@ -3,7 +3,7 @@ use async_graphql::{EmptyMutation, EmptySubscription};
 use serde::Deserialize;
 use serde::Serialize;
 use serde_json::Value;
-use sqlx::PgPool;
+use sqlx::{query_as, PgPool};
 
 const DEFAULT_LIMIT: i32 = 10;
 
@@ -13,26 +13,36 @@ pub struct QueryRoot;
 impl QueryRoot {
     async fn transactions(
         &self, ctx: &Context<'_>, limit: Option<i32>, offset: Option<i32>, hash: Option<String>,
+        job_seq_number: Option<i32>,
     ) -> Vec<Transaction> {
         let pool = ctx.data::<PgPool>().unwrap();
 
         let limit = limit.unwrap_or(DEFAULT_LIMIT);
         let offset = offset.unwrap_or(0);
 
-        let query = if let Some(ref hash) = hash {
-            sqlx::query_as::<_, Transaction>(
-                "SELECT id, body, type, hash, job_seq_number, \"to\", \"from\" FROM transactions WHERE hash = $1 LIMIT $2 OFFSET $3",
-            )
-            .bind(hash)
-            .bind(limit)
-            .bind(offset)
-        } else {
-            sqlx::query_as::<_, Transaction>(
-                "SELECT id, body, type, hash, job_seq_number, \"to\", \"from\" FROM transactions LIMIT $1 OFFSET $2",
-            )
-            .bind(limit)
-            .bind(offset)
-        };
+        let mut sql = String::from("SELECT id, body, type, hash, job_seq_number, \"to\", \"from\" FROM transactions WHERE 1=1");
+        let mut index = 1;
+
+        if hash.is_some() {
+            sql.push_str(&format!(" AND hash = ${}", index));
+            index += 1;
+        }
+        if job_seq_number.is_some() {
+            sql.push_str(&format!(" AND job_seq_number = ${}", index));
+            index += 1;
+        }
+
+        sql.push_str(&format!(" LIMIT ${} OFFSET ${}", index, index + 1));
+
+        let mut query = query_as::<_, Transaction>(&sql);
+
+        if let Some(h) = hash {
+            query = query.bind(h);
+        }
+        if let Some(jsn) = job_seq_number {
+            query = query.bind(jsn);
+        }
+        query = query.bind(limit).bind(offset);
 
         query.fetch_all(pool).await.unwrap()
     }
